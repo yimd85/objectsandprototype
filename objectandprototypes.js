6ef8ce4657c071ce8a67514ab5839bef51848c2c
yimd85 <yimd85@gmail.com>
2017-09-03 20:26:42 -0400

    Update objectandprototypes.js

--- objectandprototypes.js
+++ objectandprototypes.js
@@ -3,60 +3,50 @@ function Food(){
 }
 
 // All food can be juiced. Add a method to `juice()` the food.
-//all methods need to be lowercased. So in this case it would be "juice"
 Food.prototype.juice = function(){
-  console.log('juicing! '+ this.strain);
+  console.log('juicing! '+ this.name);
 }
-
-Food.prototype.sayName = function(){
-  console.log('I am food and my name is '+ this.strain);
-}
-
-
-// Create a `fruit` object that inherits from Food. Fruits have names. Make sure to save them.
-//Object constructors should be uppercased
-
-//one fruit has one name so 'name' would be a better argument and
-//property name. It's a small different but it makes things a lot clearer for
-//others reading your code.
-
-function Fruit(name){
-  this.name=name;
-}
-
-//where it inherits
-Fruit.prototype =Object.create(Food.prototype);
 
 // Add a method to print the name of the fruit called `sayName()`.
+Food.prototype.sayName = function(){
+  console.log('I am food and my name is '+ this.name);
+}
 
-Fruit.prototype.peel = function(){
-  console.log("skinning "+this.strain);
+// Create a `fruit` object that inherits from Food. Fruits have names. Make sure to save them.
+function Fruit(){
 }
+Fruit.prototype =Object.create(Food.prototype);
+
+
 // Create an `Apple` object that inherits from fruit.Define an `Apple` constructor that takes the name of the apple. `Apple` needs to have a boolean `hasSkin` member. Add a method to remove the skin and set `hasSkin`. Also, print the variety of the apple and tell the user the skin has been removed.Apples cannot be juiced until their skin is removed. Be careful!
-function Apple(strain){
-  this.strain=strain;
+
+function Apple(name){
+  this.name=name;
+  this.hasSkin = true
+  this.peel = function (){
+    console.log("skinning "+this.name);
+    this.hasSkin = false
+  }
 }
-//where it inherits
+
 Apple.prototype=Object.create(Fruit.prototype)
 
-Fuji = new Apple("Fuji");
-Gala = new Apple("Gala");
+var Fuji = new Apple("Fuji");
+var Gala = new Apple("Gala");
 
 // * Create a `Vegetable` object that inherits from Food. Add a method to `wash()` your vegetables. Print that the vegetable is being washed.
 
-function Vegetable(strain){
-  this.strain=strain;
+function Vegetable(name){
+  this.name=name;
   }
 //where it inherits
 Vegetable.prototype=Object.create(Food.prototype)
 
-
 Vegetable.prototype.wash = function(){
-  console.log("washing "+this.strain);
+  console.log("washing "+this.name);
 }
 // Create a `Carrot` object that inherits from `Vegetable`.
-Carrot= new Vegetable("Carrots");
-
+var Carrot= new Vegetable("Carrots");
 
 var x =
   Fuji.sayName()+
